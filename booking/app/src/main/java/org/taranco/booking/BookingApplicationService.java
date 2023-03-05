e7a68e76487c01106fdb9de7c87f9399607a893d
jakub.taran <jakub.taran@ttms.pl>
2023-03-05 15:59:50 +0100

    *Interface with main user story

---
+++ booking/app/src/main/java/org/taranco/booking/BookingApplicationService.java
@@ -0,0 +1,13 @@
+package org.taranco.booking;
+
+import org.taranco.BookingId;
+import org.taranco.HotelId;
+import org.taranco.vo.DateRange;
+
+import java.util.Set;
+
+public interface BookingApplicationService {
+    void createBooking(DateRange bookingPeriod, HotelId hotelId, Set<Room> rooms);
+
+    Booking trackBooking(BookingId bookingId);
+}
