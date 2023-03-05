0b999c2de8bb733e02157199980bdf5018dd30c0
jakub.taran <jakub.taran@ttms.pl>
2023-03-05 16:00:02 +0100

    *Separate room from booking due to reusage in app module

--- booking/domain/src/main/java/org/taranco/booking/Booking.java
+++ booking/domain/src/main/java/org/taranco/booking/Booking.java
@@ -83,24 +83,4 @@ class Booking {
     Set<Room> rooms() {
         return unmodifiableSet(rooms);
     }
-
-    static class Room {
-        static Room create(RoomId roomId, int vacancies) {
-            return new Room(roomId, vacancies);
-        }
-
-        private final RoomId roomId;
-        private final int vacancies;
-
-        private Room(RoomId roomId, int vacancies) {
-            if (roomId == null) {
-                throw new IllegalArgumentException("Room id cannot be null");
-            }
-            if (vacancies <= 0) {
-                throw new IllegalArgumentException("Cannot book empty rooms");
-            }
-            this.roomId = roomId;
-            this.vacancies = vacancies;
-        }
-    }
 }
