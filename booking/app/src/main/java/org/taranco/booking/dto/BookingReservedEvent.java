1a764c8fcaec37e5fb79391cdde8a6bc50a98d9c
jakub.taran <jakub.taran@ttms.pl>
2023-03-06 16:35:07 +0100

    *Add new event

---
+++ booking/app/src/main/java/org/taranco/booking/dto/BookingReservedEvent.java
@@ -0,0 +1,39 @@
+package org.taranco.booking.dto;
+
+import org.taranco.BookingId;
+import org.taranco.CustomerId;
+import org.taranco.DomainEvent;
+import org.taranco.vo.Money;
+
+import java.time.Instant;
+
+public class BookingReservedEvent implements DomainEvent {
+    private final Instant occurredOn;
+    private final BookingId bookingId;
+    private final Money price;
+    private final CustomerId customerId;
+
+    public BookingReservedEvent(BookingId bookingId, Money price, CustomerId customerId) {
+        this.bookingId = bookingId;
+        this.price = price;
+        this.customerId = customerId;
+        this.occurredOn = Instant.now();
+    }
+
+    @Override
+    public Instant occurredOn() {
+        return occurredOn;
+    }
+
+    public BookingId bookingId() {
+        return bookingId;
+    }
+
+    public Money price() {
+        return price;
+    }
+
+    public CustomerId customerId() {
+        return customerId;
+    }
+}
