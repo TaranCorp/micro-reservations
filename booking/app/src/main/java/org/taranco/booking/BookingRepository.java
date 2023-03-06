0e5b2c4100c8b42499fa93cb31cdc89e66eca7e3
jakub.taran <jakub.taran@ttms.pl>
2023-03-06 15:51:43 +0100

    *Add findById method

--- booking/app/src/main/java/org/taranco/booking/BookingRepository.java
+++ booking/app/src/main/java/org/taranco/booking/BookingRepository.java
@@ -1,5 +1,10 @@
 package org.taranco.booking;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface BookingRepository {
     Booking save(Booking booking);
+
+    Optional<Booking> findById(UUID id);
 }
