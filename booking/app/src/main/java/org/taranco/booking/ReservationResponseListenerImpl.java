217eab1273fe266c3639008ef76ccc35489bf31b
jakub.taran <jakub.taran@ttms.pl>
2023-03-06 23:24:20 +0100

    *Add logger and implementation of cancelled reservation logic

--- booking/app/src/main/java/org/taranco/booking/ReservationResponseListenerImpl.java
+++ booking/app/src/main/java/org/taranco/booking/ReservationResponseListenerImpl.java
@@ -1,11 +1,14 @@
 package org.taranco.booking;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.taranco.DomainEventPublisher;
 import org.taranco.NotFoundException;
 import org.taranco.booking.dto.BookingReservedEvent;
 import org.taranco.booking.dto.ReservationResponse;
 
 public class ReservationResponseListenerImpl implements ReservationResponseListener {
+    private static final Logger log = LoggerFactory.getLogger(ReservationResponseListenerImpl.class);
 
     private final BookingRepository bookingRepository;
     private final DomainEventPublisher publisher;
@@ -20,12 +23,14 @@ public class ReservationResponseListenerImpl implements ReservationResponseListe
         final Booking reservedBooking = bookingRepository.findById(response.bookingId().id())
                 .map(booking -> {
                     booking.reserve(response.hotelName(), response.price());
+                    log.info("Reserved rooms for booking with id: {}", booking.getSnapshot().bookingId().id().toString());
                     return booking;
                 })
-                .orElseThrow(() -> new NotFoundException("Cannot find Booking with id: %s. It not exists or was processed".formatted(response.bookingId().id())));
+                .orElseThrow(() -> new NotFoundException("Cannot find Booking with id: %s. It not exists or was processed".formatted(response.bookingId().id().toString())));
 
         final BookingSnapshot bookingSnapshot = bookingRepository.save(reservedBooking).getSnapshot();
 
+        log.info("Publishing BookingReservedEvent with id: {}", bookingSnapshot.bookingId().id().toString());
         publisher.publish(new BookingReservedEvent(
                 bookingSnapshot.bookingId(), bookingSnapshot.price(), bookingSnapshot.customerId()
         ));
@@ -33,6 +38,14 @@ public class ReservationResponseListenerImpl implements ReservationResponseListe
 
     @Override
     public void reservationCancelled(ReservationResponse response) {
+        final Booking cancelledBooking = bookingRepository.findById(response.bookingId().id())
+                .map(booking -> {
+                    booking.cancel();
+                    log.error("Cancelled booking with id: {}", booking.getSnapshot().bookingId().id().toString());
+                    return booking;
+                })
+                .orElseThrow(() -> new NotFoundException("Cannot find Booking with id: %s. It not exists or was processed".formatted(response.bookingId().id().toString())));
 
+        bookingRepository.save(cancelledBooking).getSnapshot();
     }
 }
