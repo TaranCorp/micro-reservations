f563caa935e92d74dc8c8f8ee43d64282e046455
jakub.taran <jakub.taran@ttms.pl>
2023-03-06 14:41:47 +0100

    *Add snapshot and recover method

--- booking/domain/src/main/java/org/taranco/booking/Booking.java
+++ booking/domain/src/main/java/org/taranco/booking/Booking.java
@@ -2,18 +2,32 @@ package org.taranco.booking;
 
 import org.taranco.BookingId;
 import org.taranco.HotelId;
-import org.taranco.RoomId;
 import org.taranco.vo.DateRange;
 import org.taranco.vo.Money;
 
 import java.time.Instant;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 import static java.util.Collections.unmodifiableSet;
 
 class Booking {
+    static Booking restore(BookingSnapshot snapshot) {
+        return new Booking(
+                snapshot.bookingId(),
+                snapshot.hotelId(),
+                snapshot.hotelName(),
+                snapshot.rooms(),
+                snapshot.bookingPeriod(),
+                snapshot.bookingDate(),
+                snapshot.paymentDate(),
+                snapshot.price(),
+                snapshot.status()
+        );
+    }
+
     private final BookingId bookingId;
     private HotelId hotelId;
     private String hotelName;
@@ -24,14 +38,26 @@ class Booking {
     private Money price;
     private BookingStatus status;
 
-    Booking(BookingId bookingId, DateRange bookingPeriod, HotelId hotelId, Set<Room> providedRooms) {
+    Booking(BookingId bookingId, DateRange bookingPeriod, HotelId hotelId, Set<Room> rooms) {
         if (hotelId == null) {
             throw new IllegalArgumentException("Hotel id cannot be null");
         }
-        addRooms(providedRooms);
+        addRooms(rooms);
         this.bookingId = bookingId;
         this.bookingPeriod = bookingPeriod;
         this.status = BookingStatus.PENDING;
+    }
+
+    Booking(BookingId bookingId, HotelId hotelId, String hotelName, Set<Room> rooms, DateRange bookingPeriod, Instant bookingDate, Instant paymentDate, Money price, BookingStatus status) {
+        this.bookingId = bookingId;
+        this.hotelId = hotelId;
+        this.hotelName = hotelName;
+        this.rooms = rooms;
+        this.bookingPeriod = bookingPeriod;
+        this.bookingDate = bookingDate;
+        this.paymentDate = paymentDate;
+        this.price = price;
+        this.status = status;
     }
 
     Booking reserve(String hotelName) {
@@ -82,5 +108,32 @@ class Booking {
 
     Set<Room> rooms() {
         return unmodifiableSet(rooms);
+    }
+
+    BookingSnapshot getSnapshot() {
+        return new BookingSnapshot(
+                bookingId,
+                hotelId,
+                hotelName,
+                rooms,
+                bookingPeriod,
+                bookingDate,
+                paymentDate,
+                price,
+                status
+        );
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Booking booking = (Booking) o;
+        return Objects.equals(bookingId, booking.bookingId) && Objects.equals(hotelId, booking.hotelId) && Objects.equals(hotelName, booking.hotelName) && Objects.equals(rooms, booking.rooms) && Objects.equals(bookingPeriod, booking.bookingPeriod) && Objects.equals(bookingDate, booking.bookingDate) && Objects.equals(paymentDate, booking.paymentDate) && Objects.equals(price, booking.price) && status == booking.status;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookingId, hotelId, hotelName, rooms, bookingPeriod, bookingDate, paymentDate, price, status);
     }
 }
